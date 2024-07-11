f5b65d65f8ca70977085c0881682e9c78106c088
leeheen <gatherworld0506@gmail.com>
2024-07-11 18:47:47 +0900

    codeup_2651(pluning)

--- 1.py
+++ 1.py
@@ -1,21 +1,15 @@
-n = int(input())
-needs = list(map(int,input().split()))
-limits = int(input())
-needs.sort()
-answer = 0
-left = 0
-right = needs[n-1]
-while left <= right:
-    mid = (left+right) // 2
-    sums = 0
-    for i in needs:
-        if i < mid:
-            sums += i
-        else:
-            sums += mid
-    if sums <= limits:
-        answer = mid
-        left = mid + 1
-    else:
-        right = mid - 1
-print(answer)
+import sys
+sys.setrecursionlimit(10**6)
+n,k = map(int,input().split())
+cnt = 0
+def f(idx,chair):
+    global cnt
+    if idx == n and chair == k:
+        cnt += 1
+        return
+    if idx >= n or chair > k:
+        return
+    f(idx+1,chair)
+    f(idx+1,chair+1)
+f(0,0)
+print(cnt)
